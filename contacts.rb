d53f8c4d9f594a2a59a51925271d48245bdc5cf0
Otieno Harrison Omondi <ogaloharrison@gmail.com>
2022-08-21 23:40:58 +0300

    commit tree 5fda12be724bb8e42b81cd8f74f229bf85d960b7

--- contacts.rb
+++ contacts.rb
@@ -19,6 +19,19 @@ end
 
 def remove_strawberry(contacts)
   # your code here!
+  # per2 = contacts["Freddy Mercury"]
+  # flavors=per2[:favorite_ice_cream_flavors]
+  # flavors.slice(0)
+  contacts.each do |person, contact_details_hash|
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        binding.pry
+        end
+      end
+    end
+    
+  end
+
 end
 
 # print the output to the terminal for inspection
